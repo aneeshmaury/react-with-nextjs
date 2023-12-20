1c3428cf0fcc4210c51420a16eb33ef5717d4aa6
Aneesh kumar maury <aneeshmaury.in@gmail.com>
2023-12-20 18:56:57 +0530

    social link clr change

--- app/page.js
+++ app/page.js
@@ -14,7 +14,7 @@ export default function Home() {
  <div className="hoverrating absolute left-[850px] top-[500px] bg-zinc-300 h-12 w-60 rounded-lg pl-5 pt-3">
   < HoverRating/>
  </div>
- <div className="sociallink text-orange-300 absolute left-[100px] top-[500px]">
+ <div className="sociallink text-red-300 absolute left-[100px] top-[500px]">
   <Sociallink/>
  </div>
  
